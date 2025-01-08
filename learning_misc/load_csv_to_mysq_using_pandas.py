b45b5b13eec3e00eb1658e8a2eb5c1ecceb6416d
ishan-kumar <ishan.kumar@justeattakeaway.com>
2025-01-08 16:05:50 +0100

    Add new examples

---
+++ learning_misc/load_csv_to_mysq_using_pandas.py
@@ -0,0 +1,22 @@
+# con='genre,artist_name,track_name,track_id,popularity,acousticness,danceability,duration_ms,energy,instrumentalness,key,liveness,loudness,mode,speechiness,tempo,time_signature,valence'
+#
+# with open('file.txt','w') as file:
+#     for word in con.split(','):
+#         file.write(f'{word} text,' )
+#         file.write('\n')
+
+
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Database connection
+engine = create_engine('mysql+pymysql://tableau:tableau@localhost:3306/spotify')
+
+# Load CSV into a DataFrame
+df = pd.read_csv('/Users/ishan.kumar/PycharmProjects/learning_project/learning_kaggle_python/data/spotify_features.csv')
+
+# Insert DataFrame into MySQL table
+df.to_sql('spotify', con=engine, if_exists='replace', index=False)
+
+print("Data loaded successfully!")

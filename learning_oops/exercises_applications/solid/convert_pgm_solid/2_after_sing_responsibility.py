53bdd2ebd170d6a3c5667bf8853a8bcbe48e461b
ishan-kumar <ishan.kumar@justeattakeaway.com>
2025-02-27 11:00:13 +0100

    adding learning files

---
+++ learning_oops/exercises_applications/solid/convert_pgm_solid/2_after_sing_responsibility.py
@@ -0,0 +1,50 @@
+#he classes, methods should do only a single thing.
+# This helps the class or methods to be used very easily.
+
+#the pay method should be mved out from the order class
+
+class Order:
+    items = []
+    quantities = []
+    prices = []
+    status = 'open'
+
+    def add_item(self, name, quantity, price):
+        self.items.append(name)
+        self.quantities.append(quantity)
+        self.prices.append(price)
+
+    def total_price(self):
+        total = 0
+        for i in range(len(self.prices)):
+            total += self.prices[i] * self.quantities[i]
+
+        return total
+
+class PaymentProcessor:
+    def pay(self, payment_type: str, security_code: str, order: Order):
+        print(f'Paying for order {order.__class__.__name__}')
+        if payment_type == 'credit':
+            print('payment type is credit')
+            print(f'Verifying security code: {security_code}')
+
+        elif payment_type == 'debit':
+            print('payment type is debit')
+            print(f'Verifying security code: {security_code}')
+
+        else:
+            raise Exception('payment type is not valid')
+
+if __name__ == '__main__':
+    order = Order()
+    order.add_item('A', 10, 100)
+    order.add_item('B', 10, 100)
+    order.add_item('C', 10, 100)
+
+    print(order.total_price())
+
+
+    p_payment = PaymentProcessor()
+    p_payment.pay(payment_type='credit', security_code='A', order=order)
+
+
---
+++ learning_oops/exercises_applications/solid/convert_pgm_solid/6_after_segregations.py
@@ -0,0 +1,86 @@
+# now lets add a add new abstract method in payment to have 2-factor authentications
+from abc import ABC, abstractmethod
+
+
+class Order:
+    items = []
+    quantities = []
+    prices = []
+    status = 'open'
+
+    def add_item(self, name, quantity, price):
+        self.items.append(name)
+        self.quantities.append(quantity)
+        self.prices.append(price)
+
+    def total_price(self):
+        total = 0
+        for i in range(len(self.prices)):
+            total += self.prices[i] * self.quantities[i]
+
+        return total
+
+
+class Payment(ABC):
+
+    @abstractmethod
+    def pay(self):
+        pass
+
+class PaymentProcessMultiFactor(Payment):
+    @abstractmethod
+    def auth_sms(self, code):
+        pass
+
+class DebitPayment(PaymentProcessMultiFactor):
+    def __init__(self, security_code):
+        self.is_multi_factor_auth = None
+        self.type = 'debit'
+        self.security_code = security_code
+        self.verified = False
+
+    def auth_sms(self, code):
+        self.is_multi_factor_auth = True
+        self.verified = True
+
+    def pay(self):
+        print(f'payment type is {self.type}')
+        print(f'Verifying security code: {self.security_code}')
+
+
+class CreditPayment(Payment):
+    def __init__(self, security_code):
+        self.type = 'credit'
+        self.security_code = security_code
+        self.verified = False
+
+    def pay(self):
+        print(f'payment type is {self.type}')
+        print(f'Verifying security code: {self.security_code}')
+
+
+class PaypalPayment(PaymentProcessMultiFactor):
+    def __init__(self, email_address):
+        self.verified = None
+        self.is_multi_factor_auth = None
+        self.type = 'paypal'
+        self.email_address = email_address
+
+    def auth_sms(self, code):
+        self.is_multi_factor_auth = True
+        self.verified = True
+
+    def pay(self):
+        print(f'payment type is {self.type}')
+        print(f'Verifying email address code: {self.email_address}')
+
+if __name__ == '__main__':
+    order = Order()
+    order.add_item('A', 10, 100)
+    order.add_item('B', 10, 100)
+    order.add_item('C', 10, 100)
+
+    print(order.total_price())
+
+    paypal_payment = PaypalPayment(email_address='abc@somwe')
+    paypal_payment.pay()

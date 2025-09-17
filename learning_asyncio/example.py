793b8566b69bfabded70d1731a735390938bb3cc
ishan-kumar <ishan.kumar@justeattakeaway.com>
2025-09-17 06:42:18 +0200

    learning

--- learning_asyncio/example.py
+++ learning_asyncio/example.py
@@ -8,7 +8,7 @@ a = list()
 async def download_task():
     print("Downloading...")
     a.append(("download", str(datetime.datetime.now())))
-    await asyncio.sleep(10)
+    await asyncio.sleep(50)
     a.append(("download after sleep", str(datetime.datetime.now())))
 
 
@@ -39,8 +39,31 @@ async def main():
 
     await download_task
 
+    print("Done after the download task is complete.")
+
+
+# 1. The download_task will be put in the event loop and will start executing.
+# 2. The log_activity will also be put in the event loop and will start executing
+# 3. The normal() function will be waited until the log_activity is done.
+# 4. Now the download_task may have been completed or not.
+# 5. if the download_task is not completed, the next print statement will wait until it is completed.
+
+# Below is the expected output:
+# (learning-project-py3.10) ➜  learning_asyncio git:(main) ✗ poetry run  python example.py
+# Downloading...
+# Logging...
+# Normal-0.
+# Normal-1.
+# Normal-2.
+# Normal-3.
+# Normal-4.
+# Normal-5.
+# Normal-6.
+# Normal-7.
+# Normal-8.
+# Normal-9.
+# Downloading completed
+# Done after the download task is complete.
 
 if __name__ == "__main__":
     asyncio.run(main())
-    for i in a:
-        print(i)
